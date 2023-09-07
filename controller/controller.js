5c26060ae7465cdf6e2c0c86dc48ccf610d41026
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-09-07 12:07:36 +0100

    final fix

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,5 @@
 const sc = require("http-status-codes")
+const moment = require("moment")
 
 
 const taskone = async (req,res)=>{
@@ -6,11 +7,11 @@ const taskone = async (req,res)=>{
     var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var currentDayOfWeek = date.getDay();
     const Current = daysOfWeek[currentDayOfWeek]
-    var currentDateUTCPlus2 = new Date(date.getTime() + 2 * 60 * 60 * 1000);
+    const currentUtcTime = moment().utcOffset(0).format('YYYY-MM-DDTHH:mm:ss[Z]');
     const returnobj = {
         "slack_name":"OlaniyiOluwaseyi",
         "current_day": Current,
-        "utc_time": currentDateUTCPlus2,
+        "utc_time": currentUtcTime,
         "track":"backend",
         "github_file_url":"https://github.com/Hydradaboss/NGX_taskone/blob/main/app.js",
         "github_repo_url":"https://github.com/Hydradaboss/NGX_taskone",
